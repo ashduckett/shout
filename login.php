2432520a981774b3c9822c444a779a27443b3622
Ash Duckett <ash.duckett@outlook.com>
2016-02-29 23:31:19 +0000

    Login page style changes.

--- login.php
+++ login.php
@@ -7,34 +7,81 @@
     <head>
         <meta charset="utf-8" />
         <title></title>
+
+        <style>
+            p {
+            /*    background-color: red; */
+                width: 100%;
+                padding-left: 7px;
+            }
+            
+            label {
+                width: 20%;
+        
+                display: inline-block;
+            }
+            
+            .container {
+                width: 940px;
+                margin: auto;
+              
+            }
+     
+            
+            form {
+ 
+                width: 40%;
+             
+       
+                margin: auto;
+                   margin-top: 400px;
+            }
+            
+ 
+            
+            .glowing-border {
+    border: 2px solid #dadada;
+    border-radius: 7px;
+    height: 20px;
+}
+
+.glowing-border:focus { 
+    outline: none;
+    border-color: #9ecaed;
+    box-shadow: 0 0 10px #9ecaed;
+}
+        </style>
+
     </head>
     <body>
         <!-- Find out how to lay out forms properly -->
         
-        <h1>Please log in...</h1>
-        <div class="login-form">
+        <div class="container">
+
+        
+      
+            
             <form action="handle_login.php" method="post">
                 <p>
                     <label>Email</label>
-                    <input type="text" name="email">
+                    <input type="text" name="email" class="glowing-border">
                 </p>
                 <p>
                     <label>Password</label>
-                    <input type="password" name="password">
+                    <input type="password" name="password" class="glowing-border">
                 </p>
                 <p>
-                    <label>Password</label>
                     <input type="submit" name="submit">
                 </p>
-
+                <p>
+                    Don't have an account? <a href="registration_page.php">Register here</a>
+                </p>
             </form>
 
-            <p>
-                Don't have an account? <a href="registration_page.php">Register here</a>
-            </p>
+    
         
-        </div>
-
+      
+            </div>
 
 
 
