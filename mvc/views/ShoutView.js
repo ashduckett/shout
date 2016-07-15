c98f5647883ee82977b08b40a23041ea3cd13ace
Ash Duckett <ash.duckett@ocsi.co.uk>
2016-07-15 15:58:45 +0100

    Made the next and previous buttons look more like buttons for the shout view. Also added disabled a disabled look to the buttons where necessary.

--- mvc/views/ShoutView.js
+++ mvc/views/ShoutView.js
@@ -22,10 +22,8 @@ ShoutView.prototype.draw = function () {
 
     // This seems to be the only way to clear the table
     $('.shout-table').empty();
+
     var _this = this;
-
-
-    // Now we need a pair of events for the next and prev buttons.
 
     var shoutTable = document.createElement('table');
     var shoutTableHeader = document.createElement('thead');
@@ -80,7 +78,7 @@ ShoutView.prototype.draw = function () {
     });
 
     $(_this.element).css('width', '50%');
-    $(_this.element).css('padding', '10px');
+
 
     $(shoutTable).css('border', '1px solid black');
     $(shoutTable).css('width', '100%');
@@ -91,12 +89,58 @@ ShoutView.prototype.draw = function () {
     var buttonBar = document.createElement('div');
     buttonBar.classList.add('button-bar');
 
+
+
     _this.previousButton = document.createElement('a');
     _this.previousButton.href = "#";
-    _this.previousButton.innerHTML = "Previous";
+    _this.previousButton.id = "prev-shout-page";
+    _this.previousButton.style.width = "50px";
+    _this.previousButton.style.textAlign = "center";
+    _this.previousButton.classList.add('fa');
+    _this.previousButton.classList.add('fa-step-backward');
+    //_this.previousButton.classList.add('btn-primary');
+
+
+    // You can't do this here because it'll overwrite when you go to the next page! Which might make it easier if we know what page we're on...
+    //$(_this.previousButton).addClass('btn-primary-disabled');
+
+    // I think this isn't working because it's already got the primary class
+    //_this.previousButton.classList.add('btn-primary-disabled');
+
     _this.nextButton = document.createElement('a');
     _this.nextButton.href = "#";
-    _this.nextButton.innerHTML = "Next";
+    _this.nextButton.id = "next-shout-page";
+    _this.nextButton.style.display = "inline-block";
+    _this.nextButton.style.width = "50px";
+    _this.nextButton.style.textAlign = "center";
+    _this.nextButton.classList.add('btn-primary');
+    _this.nextButton.classList.add('fa');
+    _this.nextButton.classList.add('fa-step-forward');
+    
+    /*
+        This is hacky. Would be good to have a disable method on button objects.
+        Actually it would be good to have button objects. These are just links.
+        It works though.
+    */
+    if (_this.model.currentPage == 1) {
+        $(_this.previousButton).addClass('btn-primary-disabled');
+    }
+
+    if (_this.model.currentPage == _this.model.totalNumberOfPages) {
+        $(_this.nextButton).removeClass('btn-primary');
+        $(_this.nextButton).addClass('btn-primary-disabled');
+
+        $(_this.previousButton).removeClass('btn-primary-disabled');
+        $(_this.previousButton).addClass('btn-primary');
+    }
+
+    if(_this.model.totalNumberOfPages == 0) {
+        $(_this.nextButton).removeClass('btn-primary');
+        $(_this.nextButton).addClass('btn-primary-disabled');
+
+        $(_this.previousButton).removeClass('btn-primary');
+        $(_this.previousButton).addClass('btn-primary-disabled');
+    }
     
     // Maybe these should be put into one page change event? Though you'd still need to know which one it was? Maybe.
     // Get the id here.
