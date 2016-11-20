e1e78c44d7b61149b4958ec2e76c70326b6183e7
Ash Duckett <ash.duckett@outlook.com>
2016-11-20 15:49:04 +0000

    Splitpane plugin can now work out what width the left side should be without being told explicitly.

--- plugins/SplitterView/SplitterView.js
+++ plugins/SplitterView/SplitterView.js
@@ -1,5 +1,5 @@
 jQuery.fn.splitterView = function () {
-
+    var context = this;
     // Create the splitter and add it to the page
     var splitter = $(document.createElement('div'));
 
@@ -15,21 +15,8 @@ jQuery.fn.splitterView = function () {
     var clickY = null;
 
     var elementBeingDragged = null;
-    var parentWidth = right.parent().width();
-
-    // Left is 17...because the content hasn't loaded at that point.
-    // When the content is loaded, the size of the left pane changes.
-    // At which time, the 17px reported here is invalid.
-
-    // How can you get hold of this 133 before the content's loaded?
-    // Maybe I need MVC stuff...
-
-    // I think you need to notify the splitpane control that data has been loaded
-
-    var amountToMinus = $('.splitter').width() + $('.left').width();
-
-
-    $('.right').css('width', 'calc(100% - ' + amountToMinus + 'px)');
+    var amountToMinus = splitter.width() + left.width();
+    right.css('width', 'calc(100% - ' + amountToMinus + 'px)');
 
 
     $(splitter).mousedown(function (event) {
@@ -53,20 +40,13 @@ jQuery.fn.splitterView = function () {
 
                 var position = $(elementBeingDragged).position();
 
-                // This value should be the value of the left of the element being dragged
+                left.width(position.left - this.offset().left);
 
-                $('.left').width(position.left - 200);
-
-                var parentWidth = $('.right').parent().width();
-
-
-                var amountToMinus = $('.splitter').width() + $('.left').width();
-
-
-                $('.right').css('width', 'calc(100% - ' + amountToMinus + 'px)');
+                var amountToMinus = splitter.width() + left.width();
+                right.css('width', 'calc(100% - ' + amountToMinus + 'px)');
             }
         }
-    });
+    }.bind(this));
 
 
 
